73e6dc8b5b084ba1a243a6d2b78c30851309df9e
nerpyshka <71436617+ValeriaNigametzianova@users.noreply.github.com>
2024-02-11 16:32:42 +0300

    Добавление в список новых слов

--- JS/page.js
+++ JS/page.js
@@ -9,8 +9,9 @@ document.getElementById('add_new_word').addEventListener('click', async (e) => {
     if (list) {
       if (list.includes(category)) return
       list.push(category)
-      chrome.storage.sync.set({ [word]: list })
-    } else chrome.storage.sync.set({ [word]: [category] })
+      await chrome.storage.sync.set({ [word]: list })
+    } else await chrome.storage.sync.set({ [word]: [category] })
+    addListItem(word, category)
   }
 })
 
@@ -23,3 +24,30 @@ chrome.storage.sync.onChanged.addListener(() => {
     console.log('storage: ', allkeys)
   })
 })
+
+const addListItem = (word, category) => {
+  let list = document.getElementById('list')
+
+  let item = document.createElement('div')
+  item.className = 'item'
+
+  let item_word = document.createElement('div')
+  item_word.innerHTML = word
+  item_word.className = 'item_word main-text'
+
+  let item_category = document.createElement('div')
+  item_category.innerHTML = category
+  item_category.className = 'item_category main-text'
+
+  let actions = document.createElement('div')
+  actions.className = 'actions'
+
+  let edit_icon = document.createElement('OBJECT')
+  edit_icon.setAttribute('data', '/Icons/Edit.svg')
+
+  actions.append(edit_icon)
+  item.append(item_word)
+  item.append(item_category)
+  item.append(actions)
+  list.append(item)
+}
