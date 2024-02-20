74b6883f95655adb80bdb80f420ded94f9523d58
nerpyshka <valeria103020@gmail.com>
2024-02-20 22:29:26 +0300

    Добавила функцию внедрения/удаления content-scripts (on/off)

--- src/background.jsx
+++ src/background.jsx
@@ -1,9 +1,1 @@
 console.log('CONNECTED')
-
-chrome.storage.sync.onChanged.addListener((event) => {
-  if (event.status) {
-    if (event.status.newValue == false) chrome.action.disable()
-    else chrome.action.enable()
-    console.log(event.status, 'event status')
-  }
-})
--- src/content.jsx
+++ src/content.jsx
@@ -2,16 +2,49 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Scripts } from './pages/Scripts'
 
-const root = document.createElement('div')
-root.id = 'crx-root'
-document.body.prepend(root)
+let extensionStatus = false
 
-ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <Scripts
-      style={{
-        display: 'flex',
-      }}
-    />
-  </React.StrictMode>
-)
+chrome.storage.sync.onChanged.addListener((changes) => {
+  if (changes.status) {
+    const newValue = changes.status.newValue
+    console.log('Extension status changed:', newValue)
+    if (newValue) {
+      injectExtension()
+    } else {
+      removeExtension()
+    }
+  }
+})
+
+function injectExtension() {
+  console.log('Injecting extension...')
+  const root = document.createElement('div')
+  root.id = 'crx-root'
+  document.body.prepend(root)
+
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <Scripts
+        style={{
+          display: 'flex',
+        }}
+      />
+    </React.StrictMode>
+  )
+}
+
+function removeExtension() {
+  console.log('Removing extension...')
+  const root = document.getElementById('crx-root')
+  if (root) {
+    root.remove()
+  }
+}
+
+// Check initial extension status
+chrome.storage.sync.get('status', (data) => {
+  extensionStatus = data.status
+  if (extensionStatus) {
+    injectExtension()
+  }
+})
