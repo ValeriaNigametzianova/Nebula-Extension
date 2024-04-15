38c0849f0fa5bbb51bd164857400225bb22bb96f
nerpyshka <71436617+ValeriaNigametzianova@users.noreply.github.com>
2024-04-15 15:38:04 +0300

    Заработало наведение на скрытый блок

--- src/components/content/HiddenBlock.jsx
+++ src/components/content/HiddenBlock.jsx
@@ -5,50 +5,68 @@ import { Dots } from '../effects/Dots'
 
 export const HiddenBlock = ({ node }) => {
   const nodeRef = useAppendChild(node)
-  const [blurColor, setBlurColor] = useState('')
-  const [blurDegree, setBlurDegree] = useState('')
   const [visibility, setVisibility] = useState(false)
   const [blurSettings, setBlurSettings] = useState(null)
 
   useEffect(() => {
     chrome.storage.sync.get(['blur_settings']).then(({ blur_settings }) => {
-      setBlurColor(blur_settings.blur_color)
-      setBlurDegree(blur_settings.blur_degree)
       setBlurSettings(blur_settings)
-      console.log(node.offsetHeight, 'node.current.offsetHeight')
     })
   }, [])
 
   return (
-    <div style={{ position: 'relative' }}>
+    <div style={{ position: 'relative', overflow: 'hidden' }}>
       <div
-        className={'nebula_mask_wrapper'}
-        style={
+        className={
           visibility
             ? undefined
-            : {
-                backgroundColor: blurColor,
-                filter: `blur(${blurDegree / 8}px)`,
-              }
+            : `nebula_mask_wrapper ${
+                blurSettings?.hover_behavior === 'zoom'
+                  ? 'zoom'
+                  : blurSettings?.hover_behavior === 'blur'
+                    ? 'blur'
+                    : ''
+              }`
         }
       >
-        <div ref={nodeRef}></div>
-      </div>
-      <div>
-        {blurSettings?.effect === 'dots_effect' ? (
-          <Dots
-            effectColor={blurSettings?.effect_color}
-            previewHeight={node.offsetHeight}
-            previewWidth={node.offsetWidth}
-          />
-        ) : null}
-        {blurSettings?.effect === 'flowers_effect' ? (
-          <Flowers
-            effectColor={blurSettings?.effect_color}
-            previewHeight={node.offsetHeight}
-            previewWidth={node.offsetWidth}
-          />
-        ) : null}
+        <div
+          style={
+            visibility
+              ? undefined
+              : {
+                  backgroundColor: blurSettings?.blur_color,
+                  filter: `blur(${blurSettings?.blur_degree / 8}px)`,
+                }
+          }
+        >
+          <div ref={nodeRef}></div>
+        </div>
+        {visibility ? undefined : (
+          <div
+            style={{
+              overflow: 'hidden',
+              width: '100%',
+              height: '100%',
+              top: '0',
+              left: '0',
+            }}
+          >
+            {blurSettings?.effect === 'dots_effect' ? (
+              <Dots
+                effectColor={blurSettings?.effect_color}
+                previewHeight={node.offsetHeight}
+                previewWidth={node.offsetWidth}
+              />
+            ) : null}
+            {blurSettings?.effect === 'flowers_effect' ? (
+              <Flowers
+                effectColor={blurSettings?.effect_color}
+                previewHeight={node.offsetHeight}
+                previewWidth={node.offsetWidth}
+              />
+            ) : null}
+          </div>
+        )}
       </div>
 
       <button
