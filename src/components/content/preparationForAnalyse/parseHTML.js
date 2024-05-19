0d5f369ddcacc5d4afe2d1845e72e57a2ecb23a7
DGAFBB <71436333+DGAFBB@users.noreply.github.com>
2024-05-19 23:28:21 +0300

    Скорректировала XPath

--- src/components/content/preparationForAnalyse/parseHTML.js
+++ src/components/content/preparationForAnalyse/parseHTML.js
@@ -3,15 +3,12 @@ import { getElementsArray } from './getElementsArray'
 export const parseHTML = (wordList, elementsArray) => {
   for (let key in wordList) {
     const headings = document.evaluate(
-      // `.//*[contains(text(), "${key}")]`,
-      `.//*[contains(text(), "${key}") and not(contains(@class, 'nebula'))]`,
-      // `.//*[not(contains(@class, 'nebula'))]`,
+      `.//*[contains(text(), "${key}")]`,
       document.body,
       null,
-      XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+      XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
       null
     )
-
     for (let i = 0, length = headings.snapshotLength; i < length; ++i) {
       getElementsArray(headings.snapshotItem(i), elementsArray)
     }
