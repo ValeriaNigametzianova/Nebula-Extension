58c865c627041426385729ab636f441bb3757941
DGAFBB <71436333+DGAFBB@users.noreply.github.com>
2024-05-21 19:45:38 +0300

    Update useLogAllKeys.js

--- src/components/content/hooks/useLogAllKeys.js
+++ src/components/content/hooks/useLogAllKeys.js
@@ -1,9 +1,15 @@
 import { useEffect } from 'react'
-
+function getAllKeys() {
+  chrome.storage.sync.get(null, (allkeys) => {
+    console.log('allkeys: ', allkeys)
+  })
+}
 export const useLogAllKeys = () => {
   useEffect(() => {
-    chrome.storage.sync.get(null, (allkeys) => {
-      console.log('allkeys: ', allkeys)
-    })
-  })
+    chrome.storage.sync.onChanged.addListener(getAllKeys)
+
+    return () => {
+      chrome.storage.sync.onChanged.removeListener(getAllKeys)
+    }
+  }, [])
 }
