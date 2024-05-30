cd49d053ea5649c3c6c393451412bf530765582c
DGAFBB <71436333+DGAFBB@users.noreply.github.com>
2024-05-30 14:01:30 +0300

    Добавила транслитерацию

--- src/components/content/preparationForAnalyse/parseHTML.js
+++ src/components/content/preparationForAnalyse/parseHTML.js
@@ -1,12 +1,17 @@
 import { getElementsArray } from './getElementsArray'
+import { transliterate } from './transliterate'
 
 export const parseHTML = (wordList, elementsArray) => {
+  console.log(1)
   for (let word in wordList) {
     const stemms = wordList[word].stemms
+    const transliteration = transliterate(stemms)
     if (stemms)
-      stemms.map((stemma) => {
+      for (let i = 0; i < stemms.length; i++) {
+        const stemma = stemms[i]
+        const transliterateWord = transliteration[i]
         const headings = document.evaluate(
-          `.//*[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ', 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя'), "${stemma}")]`,
+          `.//*[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ', 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя'), "${stemma}") or contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ', 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя'), "${transliterateWord}")]`,
           document.body,
           null,
           XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
@@ -15,7 +20,7 @@ export const parseHTML = (wordList, elementsArray) => {
         for (let i = 0, length = headings.snapshotLength; i < length; ++i) {
           getElementsArray(headings.snapshotItem(i), elementsArray)
         }
-      })
+      }
     else {
       const headings = document.evaluate(
         `.//*[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ', 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя'), "${word}")]`,
