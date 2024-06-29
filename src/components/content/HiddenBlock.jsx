e580d836d4a54fef97ea9caa8aecb0ac98ca21ae
nerpyshka <71436617+ValeriaNigametzianova@users.noreply.github.com>
2024-06-29 21:05:32 +0300

    Изменила присвоение названия класса

--- src/components/content/HiddenBlock.jsx
+++ src/components/content/HiddenBlock.jsx
@@ -15,8 +15,12 @@ export const HiddenBlock = ({ node, word, category }) => {
       setBlurSettings(blur_settings)
     })
   }, [])
+
   return (
-    <div style={{ position: 'relative', overflow: 'hidden' }}>
+    <div
+      className="nebula"
+      style={{ position: 'relative', overflow: 'hidden' }}
+    >
       <div
         className={
           visibility
@@ -31,6 +35,7 @@ export const HiddenBlock = ({ node, word, category }) => {
         }
       >
         <div
+          className="nebula"
           style={
             visibility
               ? undefined
@@ -42,40 +47,44 @@ export const HiddenBlock = ({ node, word, category }) => {
         >
           <div ref={nodeRef}></div>
         </div>
-        {visibility ? undefined : (
-          <div
-            style={{
-              overflow: 'hidden',
-              width: '100%',
-              height: '100%',
-              top: '0',
-              left: '0',
-            }}
-          >
-            {blurSettings?.effect === 'dots_effect' ? (
-              <Dots
-                effectColor={blurSettings?.effect_color}
-                previewHeight={node.offsetHeight}
-                previewWidth={node.offsetWidth}
-              />
-            ) : null}
-            {blurSettings?.effect === 'flowers_effect' ? (
-              <Flowers
-                effectColor={blurSettings?.effect_color}
-                previewHeight={node.offsetHeight}
-                previewWidth={node.offsetWidth}
-              />
-            ) : null}
-            {(blurSettings?.show_word || blurSettings?.show_category) && (
-              <ShowMaskedWord
-                word={word}
-                category={category}
-                showWord={blurSettings?.show_word}
-                showCategory={blurSettings?.show_category}
-              ></ShowMaskedWord>
-            )}
-          </div>
-        )}
+
+        <div
+          className="nebula"
+          style={
+            visibility
+              ? undefined
+              : {
+                  overflow: 'hidden',
+                  width: '100%',
+                  height: '100%',
+                  top: '0',
+                  left: '0',
+                }
+          }
+        >
+          {blurSettings?.effect === 'dots_effect' ? (
+            <Dots
+              effectColor={blurSettings?.effect_color}
+              previewHeight={node.offsetHeight}
+              previewWidth={node.offsetWidth}
+            />
+          ) : null}
+          {blurSettings?.effect === 'flowers_effect' ? (
+            <Flowers
+              effectColor={blurSettings?.effect_color}
+              previewHeight={node.offsetHeight}
+              previewWidth={node.offsetWidth}
+            />
+          ) : null}
+          {(blurSettings?.show_word || blurSettings?.show_category) && (
+            <ShowMaskedWord
+              word={word}
+              category={category}
+              showWord={blurSettings?.show_word}
+              showCategory={blurSettings?.show_category}
+            ></ShowMaskedWord>
+          )}
+        </div>
       </div>
       <div className="nebula_hidden_block_show_options">
         <ShowContentButton onClick={setVisibility} visibility={visibility} />
